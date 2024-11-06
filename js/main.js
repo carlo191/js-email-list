52e36b0421cc62bac591dffd8dd624ecf93f6c33
Carlo Elvisi <carloelvisi01@outlook.it>
2024-11-06 15:43:09 +0100

    email si generano in pagina

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 // RACCOLTA DATI
+
 const emailsList = document.getElementById("email-list");
 
 const creaSingolaEmail = () => {
-  fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((Response) => Response.json())
-    .then((data) => {
-      console.log(data.response);
-    });
+  return fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => response.json())
+    .then((data) => data.response);
 };
 
-const emails = [];
+// Array   10 email
+const emailsArray = [];
 for (let i = 0; i < 10; i++) {
-  emails.push(creaSingolaEmail());
+  emailsArray.push(creaSingolaEmail());
 }
+
+Promise.all(emailsArray).then((emails) => {
+  emails.forEach((email) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = email;
+    emailsList.appendChild(listItem);
+  });
+});
